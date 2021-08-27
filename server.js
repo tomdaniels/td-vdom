d543f4bb23c00eec1a44b7600941bab2adce154e
tomdaniels <thomsondaniels@hotmail.com>
2021-08-27 22:09:38 +1000

    express

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-const app = express();
-
 const port = 8080;
+const app = express();
 
 app.use(express.static('./'));
 
-// Our first route
 app.get('/', (req, res) => {
   res.sendFile('./index.html');
 });
